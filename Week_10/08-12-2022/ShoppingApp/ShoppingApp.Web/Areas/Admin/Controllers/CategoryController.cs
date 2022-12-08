92be678c1fc4c7eb6a8c7cfba077812e8f1fdc41
CemreSenel <114918315+CemreSenel@users.noreply.github.com>
2022-12-08 18:05:06 +0300

    08.12.2022

---
+++ Week_10/08-12-2022/ShoppingApp/ShoppingApp.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingApp.Business.Abstract;
+using ShoppingApp.Web.Areas.Admin.Models.Dtos;
+
+namespace ShoppingApp.Web.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories =await _categoryService.GetAllAsync();
+            var categoryListDto = new CategoryListDto
+            {
+                Categories = categories
+            };
+            return View(categoryListDto);
+        }
+    }
+}
---
+++ Week_10/08-12-2022/ShoppingApp/ShoppingApp.Web/Areas/Admin/Models/Dtos/CategoryListDto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingApp.Entity.Concrete;
+
+namespace ShoppingApp.Web.Areas.Admin.Models.Dtos
+{
+    public class CategoryListDto : Controller
+    {
+        public List<Category>Categories { get;set; }
+    }
+}
