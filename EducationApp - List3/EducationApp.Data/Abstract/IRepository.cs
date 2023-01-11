64007196b6161869e9a764b373e9be7c089854a8
CemreSenel <114918315+CemreSenel@users.noreply.github.com>
2023-01-11 14:49:36 +0300

    11.01.2023

---
+++ EducationApp - List3/EducationApp.Data/Abstract/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationApp.Data.Abstract
+{
+    public interface IRepository<T>
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task CreateAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ EducationApp - List3/EducationApp.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationApp.Data.Abstract
+{
+    public interface IUnitOfWork :IDisposable
+    {
+        IStudentRepository Students { get; }
+        ICategoryRepository Categories { get; } 
+
+    }
+}
