f10091c9a498b1e93c708e54d136cfb0ff1041d9
semakocaoglu <semakocaoglu7@gmail.com>
2023-01-20 14:59:30 +0300

    20.01.2023

---
+++ 20.01.2023/EducationApp/EducationApp.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EducationApp.Web.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
