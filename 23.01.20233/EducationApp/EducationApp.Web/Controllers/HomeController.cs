4864520ca5323c855fb65a027aaf96229c1be4ea
semakocaoglu <semakocaoglu7@gmail.com>
2023-01-23 15:01:17 +0300

    23.01.2023

---
+++ 23.01.20233/EducationApp/EducationApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using EducationApp.Entity.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace EducationApp.Web.Controllers;
+
+public class HomeController : Controller
+{
+
+    public async Task<IActionResult> Index()
+    {
+        return View();
+
+    }
+
+
+}
